05c10e046fff4097cf14af67562a5992f36da6fb
jciterceros <jciterceros@yahoo.com>
2021-09-29 21:09:56 -0300

    Inicio

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*
+ dependency:
+      npm install uuid
+      npm install react-icons --save
+      npm install react-router-dom
+      npm install axios
+*/
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
