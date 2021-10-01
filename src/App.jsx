e39ecf93721012df210e02b81801181d972ad502
jciterceros <jciterceros@yahoo.com>
2021-10-01 12:26:48 -0300

    MD

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,15 @@
-import React,{useState, useEffect} from 'react';
-import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React,{useState, useEffect, Component} from 'react';
+import  { findDOMNode }  from 'react-dom'
 
-import './App.css';
 import "../node_modules/video-react/dist/video-react.css"; // import css
-import { Player } from 'video-react';
-//https://www.youtube.com/watch?v=_nBlN9yp9R8&ab_channel=CodingEntrepreneurs
 import ReactPlayer from 'react-player'
 
-
-//import { StyleSheet, View } from 'react-native';
-//import VideoPlayer from './Components/VideoPlayer';
+import './App.css';
 
 const App = () => {
 
-/*
-  Dependency:
-        https://video-react.js.org/
-  Documents:
-        https://video-react.js.org/components/player/
-
-  Aqui que comeza a brincadeira
-          <Player
-            playsInline
-            poster="/assets/poster.png"
-            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-            width= "200"
-            height = "200"
-            autoPlay
-          />
-
-
-
-            <Player
-              playsInline
-              preload={true}
-              fluid={false}
-              poster="/assets/poster.png"
-              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-              width= "320px"
-              height = "240px"
-              autoPlay={true}
-              muted={true}
-              loop={true}
-            />
-
-
-
-            <View style={styles.container}>
-              <VideoPlayer />
-            </View>
-    */
-  const message = "Hello world";
-  const videoURL = "http://localhost:3000/videos/Parcialmente Nublado _ Partly Cloudy - Parte 1.mp4";
-  const URL = "https://media.w3.org/2010/05/sintel/trailer_hd.mp4";
-
+  const lista = [];
+  
 
   const [selectedFile, setSelectedFile] = useState(null)
   const onChangeHandler = event => {
@@ -71,66 +25,25 @@ const App = () => {
   return (
     <>
       <div className="container">
-          {
-          //message
-          }
           <div className="videos">
-            <Player>
-              <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-            </Player>
-            {/*<ReactPlayer width='100%' height='50%' url='https://www.youtube.com/watch?v=nYTrIcn4rjg&ab_channel=ClipsHay' volume={0.5} />*/}
+            {
+              /*
+                <ReactPlayer width='100%' height='50%' url='https://www.youtube.com/watch?v=nYTrIcn4rjg&ab_channel=ClipsHay' volume={0.5} />
+
+              */
+            }
             <input type="file" name="file" onChange={onChangeHandler} />
             <ReactPlayer
-              width='98%' height='70%' url={selectedFile} controls
+              //width='100%' height='100%' 
+              playing
+              loop
+              url={selectedFile} controls
               volume={10/100}
             />
 
-            {/*
-            https://www.tabnine.com/code/javascript/modules/react-player
-            <div className="videos">
-              <Player>
-                <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-              </Player>
-              ////////{<ReactPlayer width='100%' height='50%' url='https://www.youtube.com/watch?v=nYTrIcn4rjg&ab_channel=ClipsHay' volume={0.5} />}
-              <input type="file" name="file" onChange={onChangeHandler} />
-              <ReactPlayer
-                width='99%' height='50%' url={selectedFile} controls
-                volume={10/100}
-              />
-
-
-              https://video-react.js.org/components/volume-menu-button/
-              https://video-react.js.org/components/player/
-
-              https://www.npmjs.com/package/react-player
-              https://cookpete.com/react-player/
-              https://github.com/MohsinGhani/react-lib-examples/blob/3d9e690857b8ee8a3711e4acef48e2146a44c9f1/src/Components/ReactPlayerExample.js#L4
-
-            <Player
-              playsInline
-              preload={true}
-              fluid={true}
-              poster="/assets/poster.png"
-              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-              autoPlay={true}
-              muted={false}
-              volume={0.2}
-              loop={true}
-            />
-            */}
           </div>
       </div>     
     </>
-  );
-};
-
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'black',
-  },
-});
-*/
+  )
+}
 export default App;
